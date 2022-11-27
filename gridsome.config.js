96bacf57a80156de8dd06f007eb9593f5fc36acb
avnikasikciwin <avni.kasikci.01@gmail.com>
2022-11-28 01:01:10 +0300

    create project

---
+++ gridsome.config.js
@@ -0,0 +1,52 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+// module.exports = {
+//   siteName: 'Gridsome',
+//   plugins: []
+// }
+
+
+module.exports = {
+  siteName: 'Avni Kasikci',
+  siteDescription: 'Full Stack Developer',  
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'blog/**/*.md',
+        typeName: 'BlogPost',
+        resolveAbsolutePaths: true,
+        remark: {
+          externalLinksTarget: '_blank',
+          externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+        },
+      },
+    },
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: 'UA-72659574-10'
+      }
+    },
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        cacheTime: 600000
+      }
+    }
+  ],
+  css: {
+    loaderOptions: {
+      scss: {
+      }
+    }
+  },
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080
+  }
+}
---
+++ src/layouts/Default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="layout">
+    <header class="header">
+      <Navbar :siteName="$static.metadata.siteName"/>
+    </header>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<script>
+import Navbar from '~/components/navbar'
+export default {
+  components:{
+    Navbar
+  }
+}
+</script>
