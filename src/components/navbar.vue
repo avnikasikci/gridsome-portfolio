508d40581a980ddb1d470d3de2292694a58ee9dc
avnikasikciwin <avni.kasikci.01@gmail.com>
2022-11-29 23:34:41 +0300

    update : desing update

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
         <!-- <nav class="navbar navbar-expand-lg "> -->
-                    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark" style="    background-color: #0c1727 !important;">
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark"
+            style="    background-color: #0c1727 !important;">
 
 
             <div class="container-fluid">
@@ -12,7 +13,7 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex">
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
@@ -44,10 +45,71 @@
                 </div>
             </div>
         </nav>
+<!-- 
+        <nav class="nav__StyledNav-ghklvd-1 kFCHyX">
+            <div class="logo fade-enter-done" tabindex="-1"><a href="/" aria-label="home"><svg id="logo"
+                        xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
+                        <title>Logo</title>
+                        <g transform="translate(-8.000000, -2.000000)">
+                            <g transform="translate(11.000000, 5.000000)">
+                                <path
+                                    d="M45.691667,45.15 C48.591667,46.1 50.691667,48.95 50.691667,52.2 C50.691667,57.95 46.691667,61 40.291667,61 L28.541667,61 L28.541667,30.3 L39.291667,30.3 C45.691667,30.3 49.691667,33.15 49.691667,38.65 C49.691667,41.95 47.941667,44.35 45.691667,45.15 Z M33.591667,43.2 L39.241667,43.2 C42.791667,43.2 44.691667,41.85 44.691667,38.95 C44.691667,36.05 42.791667,34.8 39.241667,34.8 L33.591667,34.8 L33.591667,43.2 Z M33.591667,47.5 L33.591667,56.5 L40.191667,56.5 C43.691667,56.5 45.591667,54.75 45.591667,52 C45.591667,49.2 43.691667,47.5 40.191667,47.5 L33.591667,47.5 Z"
+                                    fill="currentColor"></path>
+                                <polygon id="Shape" stroke="currentColor" stroke-width="5" stroke-linecap="round"
+                                    stroke-linejoin="round" points="39 0 0 22 0 67 39 90 78 68 78 23"></polygon>
+                            </g>
+                        </g>
+                    </svg></a></div>
+            <div class="nav__StyledLinks-ghklvd-2 buZqzj">
+                <ol>
+                    <li class="fadedown-enter-done" style="transition-delay: 0ms;"><a href="/#about">About</a></li>
+                    <li class="fadedown-enter-done" style="transition-delay: 100ms;"><a href="/#jobs">Experience</a>
+                    </li>
+                    <li class="fadedown-enter-done" style="transition-delay: 200ms;"><a href="/#projects">Work</a></li>
+                    <li class="fadedown-enter-done" style="transition-delay: 300ms;"><a href="/#contact">Contact</a>
+                    </li>
+                </ol>
+                <div class="fadedown-enter-done" style="transition-delay: 400ms;"><a class="resume-button"
+                        href="/resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a></div>
+            </div>
+            <div class="menu__StyledMenu-sdn9y6-0 dAwNpJ fade-enter-done">
+                <div><button aria-label="Menu" class="menu__StyledHamburgerButton-sdn9y6-1 eCxtrN">
+                        <div class="ham-box">
+                            <div class="ham-box-inner"></div>
+                        </div>
+                    </button>
+                    <aside aria-hidden="true" tabindex="-1" class="menu__StyledSidebar-sdn9y6-2 dKogfI">
+                        <nav>
+                            <ol>
+                                <li><a href="/#about">About</a></li>
+                                <li><a href="/#jobs">Experience</a></li>
+                                <li><a href="/#projects">Work</a></li>
+                                <li><a href="/#contact">Contact</a></li>
+                            </ol><a href="/resume.pdf" class="resume-link">Resume</a>
+                        </nav>
+                    </aside>
+                </div>
+            </div>
+        </nav> -->
 
-   
+
     </div>
 </template>
+<style>
+.kFCHyX {
+    display: flex;
+    -webkit-box-pack: justify;
+    justify-content: space-between;
+    -webkit-box-align: center;
+    align-items: center;
+    position: relative;
+    width: 100%;
+    color: var(--lightest-slate);
+    font-family: var(--font-mono);
+    counter-reset: item 0;
+    z-index: 12;
+}
+</style>
 <script>
 export default {
     props: ['siteName'],
--- src/components/rightContent.vue
+++ src/components/rightContent.vue
@@ -1,10 +1,43 @@
 <template>
-    <div id="right">
+    <!-- <div id="right">
         <ul>
             <li>tets1</li>
         </ul>
+    </div> -->
+
+    <div  id="right" orientation="right" class="side__StyledSideElement-sc-1duznzb-0 jSIwrL">
+        <div class="email__StyledLinkWrapper-sc-2epoq-0 jJFfEJ fade-enter-done"><a
+                href="mailto:brittany.chiang@gmail.com">brittany.chiang@gmail.com</a></div>
     </div>
 </template>
+<style>
+.jJFfEJ{
+    display: flex;
+    flex-direction: column;
+    -webkit-box-align: center;
+    align-items: center;
+    position: relative;
+}
+.jSIwrL {
+    width: 40px;
+    position: fixed;
+    bottom: 0px;
+    left: auto;
+    right: -7px;
+    z-index: 10;
+    color: var(--light-slate);
+}
+.jJFfEJ a {
+    margin: 20px auto;
+    padding: 10px;
+    font-family: var(--font-mono);
+    font-size: var(--fz-xxs);
+    line-height: var(--fz-lg);
+    letter-spacing: 0.1em;
+    -webkit-writing-mode: vertical-rl;
+    writing-mode: vertical-rl;
+}
+</style>
 <script>
 export default {
     props: ['siteName'],
